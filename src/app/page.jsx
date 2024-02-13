3fcbf36885798a0b6f222d545236e035fdfadd2e
m-triple-m <triplem656@gmail.com>
2024-02-13 19:47:27 +0530

    setup

---
+++ src/app/page.jsx
@@ -0,0 +1,55 @@
+'use client';
+import React, { useState } from 'react'
+
+const Home = () => {
+
+  const [now, setNow] = useState(new Date());
+
+  setInterval(() => {
+    setNow(new Date());
+  })
+
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 15 }}></span>
+          </span>
+          days
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 10 }}></span>
+          </span>
+          hours
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 24 }}></span>
+          </span>
+          min
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": now.getSeconds() }}></span>
+          </span>
+          sec
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/app/template.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Template = ({ children }) => {
+    return (
+        <>
+            {children}
+            <Footer />
+        </>
+    )
+}
+
+export default Template
